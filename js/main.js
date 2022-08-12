d8e52acd7f1e40fd836f670ecf87100227ef3ad6
juliaelviss <101476676+juliaelviss@users.noreply.github.com>
2022-08-12 21:21:24 +0300

    все норм

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(function () {
   var mixer = mixitup('.galery__content');
+
+ $('.slider__blog-inner').slick({
+   dots: true,
+   infinite: true,
+   speed: 500,
+   fade: true,
+   cssEase: 'linear',
+ });
 });
